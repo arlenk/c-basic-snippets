8d1ffa9fe07e4d4f1d1b461a92fe13a0de677e69
arlenk <arlen.khodadadi@gmail.com>
2018-06-14 22:41:56 -0400

    skeleton program to read some mailing addresses sample data

---
+++ models/mailing_address.h
@@ -0,0 +1,12 @@
+typedef struct
+{
+	int id;
+	char *first_name;
+	char *last_name;
+	char *email;
+	char *gender;
+	char *address;
+	char *city;
+	char *state;
+	int zip;
+} Address;
---
+++ programs/mailing_addresses.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#include <mailing_address.h>
+
+#define LINE_BUFFER_SIZE 2048 /* max line length we can read in */
+
+int main(int argc, char *argv[])
+{
+	if (argc < 1)
+	{
+		return 1;
+	}
+	char *input_file = argv[1];
+	printf("reading data from %s\n\n", input_file);
+	int i = 0;
+
+	FILE *input = fopen(input_file, "r");
+	Address address;
+
+	char line[LINE_BUFFER_SIZE];
+
+	while (fgets(line, LINE_BUFFER_SIZE, input))
+	{
+		// fscanf(input, "%d|%s|%s|%s|%s|%s|%s|%s|%d") == 9)
+		/* ignore header row */
+		if (i > 0) 
+		{
+			if (i % 100 == 0)
+			{
+				printf("%s\n", line);
+				printf("%lu\n", strlen(line));
+				char *token;
+				token = strtok(line, "|");
+				while (token != NULL)
+				{
+					printf("token: %s\n", token);
+					printf("%s\n", line);
+					token = strtok(NULL, "|");
+				}
+			}		
+		}
+		i++;
+	}
+	printf("read %d rows\n", i);
+	return 0;	
+}
